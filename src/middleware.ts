a7dc799ed41f25c255ed10f80e7846db32b7b797
Erik Budanov <erik.budanov@gmail.com>
2026-04-30 21:15:15 +0400

    Set middleware runtime to nodejs to avoid edge runtime limitations

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 import type { NextRequest } from 'next/server'
 
+export const runtime = 'nodejs'
+
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
 
